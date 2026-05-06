264a378d5c8a7d03b963c4083786da8d65dfc49a
azimbek <voron75voron@gmail.com>
2026-05-06 18:24:02 +0500

    added gunicorn

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -3,8 +3,9 @@ import multiprocessing
 # Bind to 0.0.0.0 for Docker/DigitalOcean App Platform
 bind = "0.0.0.0:8000"
 
-# Best practice workers count = 2 * CPU cores + 1
-workers = multiprocessing.cpu_count() * 2 + 1
+# Best practice workers count for small containers
+import os
+workers = int(os.environ.get("WEB_CONCURRENCY", 2))
 
 # Timeouts
 timeout = 30
